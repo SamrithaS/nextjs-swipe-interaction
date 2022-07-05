670732299e0fa0712bdb08d5c022be096f35f115
SamrithaS <samritha@timeless.co>
2022-07-05 10:59:00 +0530

    animation fixes

--- pages/[slug].js
+++ pages/[slug].js
@@ -12,22 +12,32 @@ const Slug = () => {
   console.log(router, "router");
   return (
     <>
-      {/* <Home /> */}
-      <div style={{ position: 'absolute' }}>
+    <motion.div style={{background: 'black', opacity: 0.5}} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}>
+      <Home />
+      </motion.div>
+      <div style={{ position: "absolute" }}>
         <Link href="/" key={slug}>
           <motion.div
             layoutId={slug}
             className={styles.card}
-            animate={{ scale: 1.2 }}
-            whileDrag={{ scale: 1.1 }}
+            initial={{ scale: 1 }}
+            whileDrag={{ scale: 0.8}}
             onDragEnd={handleDragEnd}
             dragElastic={0.6}
             dragConstraints={{ top: -133, bottom: 100 }}
             drag="y"
             transition={{ type: "tween", stiffness: 10, duration: 0.6 }}
           >
-            <motion.img layoutId={`${slug}1`}  className={styles.img} src={`${slug}.jpg`} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}/>
-            <motion.div className={styles.p} transition={{ type: "spring", stiffness: 100, duration: 0.6 }}>
+            <motion.img
+              layoutId={`${slug}img`}
+              className={styles.img}
+              src={`${slug}.jpg`}
+              transition={{ type: "tween", stiffness: 10, duration: 0.6 }}
+            />
+            <motion.div
+              className={styles.p}
+              transition={{ type: "spring", stiffness: 100, duration: 0.6 }}
+            >
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industrys standard dummy text.
               Lorem Ipsum is simply dummy text of the printing and typesetting
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   let array = ["bug", "cat", "snow", "moon"];
   return (
     <div className={styles.div}>
@@ -13,12 +15,12 @@ export default function Home() {
           <motion.div
             layoutId={item}
             className={styles.card}
-            animate={{ scale: 1 }}
+            initial={{scale:1}}
             transition={{ type: "tween", stiffness: 100, duration: 0.6 }}
           >
-            <motion.img layoutId={`${item}1`} className={styles.img} src={`${item}.jpg`} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}/>
+            <motion.img layoutId={`${item}img`} className={styles.img} src={`${item}.jpg`} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}/>
 
-            <motion.div className={styles.p} transition={{ type: "spring", stiffness: 100, duration: 0.6 }}>
+            <motion.div className={styles.p} transition={{ type: "tween", stiffness: 100, duration: 0.6 }}>
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industrys standard dummy text.
             </motion.div>
