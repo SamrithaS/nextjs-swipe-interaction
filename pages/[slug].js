33cfb794a63b5189ad2e837e3b076a04622933a1
SamrithaS <samritha@timeless.co>
2022-07-05 11:45:24 +0530

    css changes

--- pages/[slug].js
+++ pages/[slug].js
@@ -3,30 +3,32 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import styles from "../styles/Home.module.css";
 import Home from "./index";
+
 const Slug = () => {
   const router = useRouter();
   const { slug } = router.query;
   const handleDragEnd = () => {
     router.push("/");
   };
-  console.log(router, "router");
+
   return (
     <>
-    <motion.div style={{background: 'black', opacity: 0.5}} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}>
-      <Home />
-      </motion.div>
-      <div style={{ position: "absolute" }}>
+      <div style={{ background: "black", opacity: 0.5 }}>
+        <Home />
+      </div>
+      <div className={styles.wrapper}>
         <Link href="/" key={slug}>
           <motion.div
             layoutId={slug}
             className={styles.card}
             initial={{ scale: 1 }}
-            whileDrag={{ scale: 0.8}}
+            whileDrag={{ scale: 0.8 }}
             onDragEnd={handleDragEnd}
             dragElastic={0.6}
-            dragConstraints={{ top: -133, bottom: 100 }}
+            style={{ originY: 0.5 }}
+            dragConstraints={{ top: 0, bottom: 0 }}
             drag="y"
-            transition={{ type: "tween", stiffness: 10, duration: 0.6 }}
+            transition={{ type: "tween", stiffness: 100, duration: 0.6 }}
           >
             <motion.img
               layoutId={`${slug}img`}
--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,20 @@ import styles from "../styles/Home.module.css";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   let array = ["bug", "cat", "snow", "moon"];
+
+  useEffect(() => {
+    if (router.pathname !== "/") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  }, [router]);
+
   return (
     <div className={styles.div}>
       {array.map((item) => (
@@ -15,12 +25,20 @@ export default function Home() {
           <motion.div
             layoutId={item}
             className={styles.card}
-            initial={{scale:1}}
+            initial={{ scale: 1 }}
             transition={{ type: "tween", stiffness: 100, duration: 0.6 }}
           >
-            <motion.img layoutId={`${item}img`} className={styles.img} src={`${item}.jpg`} transition={{ type: "tween", stiffness: 10, duration: 0.6 }}/>
+            <motion.img
+              layoutId={`${item}img`}
+              className={styles.img}
+              src={`${item}.jpg`}
+              transition={{ type: "tween", stiffness: 10, duration: 0.6 }}
+            />
 
-            <motion.div className={styles.p} transition={{ type: "tween", stiffness: 100, duration: 0.6 }}>
+            <motion.div
+              className={styles.p}
+              transition={{ type: "tween", stiffness: 100, duration: 0.6 }}
+            >
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industrys standard dummy text.
             </motion.div>
